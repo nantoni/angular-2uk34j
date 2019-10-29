427f4b890a359f2bbe2414990f1d395dacf04144
nantoni <nathan.antoni@outlook.fr>
2019-10-29 16:53:45 +0100

    4 files modified, 13 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AccueilComponent } from './components/accueil/accueil.component';
+import { CompteComponent } from './components/compte/compte.component';
+import { CatalogueComponent } from './components/catalogue/catalogue.component';
+import { PanierComponent } from './components/panier/panier.component';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, AccueilComponent, CompteComponent, CatalogueComponent, PanierComponent, DetailComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
